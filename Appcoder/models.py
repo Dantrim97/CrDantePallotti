eb937b7144edfb7b4b085f3b8149784eb80755db
Dantrim97 <dantepallotti24@gmail.com>
2023-09-17 15:56:56 -0300

    avanzando

--- Appcoder/models.py
+++ Appcoder/models.py
@@ -11,3 +11,20 @@ class Equipo(models.Model):
     nombre= models.CharField(max_length=40)
     apellido= models.CharField(max_length=40)
     email= models.EmailField()
+
+class Socio(models.Model):
+
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    email= models.EmailField()
+
+from django.db import models
+
+class DeudaCliente(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_vencimiento = models.DateField()
+    estado = models.CharField(max_length=20, choices=(('Pendiente', 'Pendiente'), ('Pagada', 'Pagada')))
+
+    def __str__(self):
+        return f"Deuda de {self.cliente} - ${self.monto}"
